784d2fabfe68d76086f8121ff1258d4d5fda32ad
Elena Isaeva <isayeva.yelena.94@mail.ru>
2023-02-08 18:26:25 +0300

    correct tests

--- src/test/java/ru/netology/rest/MobileBankApiTestV1.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -10,21 +10,21 @@ import static org.hamcrest.Matchers.*;
 class MobileBankApiTestV1 {
     @Test
     void shouldReturnDemoAccounts() {
-      // Given - When - Then
-      // Предусловия
-      given()
-          .baseUri("http://localhost:9999/api/v1")
-      // Выполняемые действия
-      .when()
-          .get("/demo/accounts")
-      // Проверки
-      .then()
-          .statusCode(200)
-              .header("Content-Type", "application/json; charset=UTF-8")
-              .contentType(ContentType.JSON)
-              .body("",hasSize(3))
-              .body("[1].balance", greaterThanOrEqualTo(0))
-              .body("every { it.balance >= 0}",is(true));
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                // Выполняемые действия
+                .when()
+                .get("/demo/accounts")
+                // Проверки
+                .then()
+                .statusCode(200)
+                .header("Content-Type", "application/json; charset=UTF-8")
+                .contentType(ContentType.JSON)
+                .body("", hasSize(3))
+                .body("[1].balance", greaterThanOrEqualTo(0))
+                .body("every { it.balance >= 0}", is(true));
 
     }
 
@@ -38,6 +38,5 @@ class MobileBankApiTestV1 {
                 // Проверки
                 .then()
                 .body(matchesJsonSchemaInClasspath("accounts.schema.json"));
-
     }
 }
